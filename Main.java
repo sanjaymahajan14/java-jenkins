ede8532c7c135c4fcb58512174de6b31897dab60
sanjaymahajan14 <sanjaymahajan72@gmail.com>
2025-10-01 22:51:30 +0530

    This is Third commit

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main{
         System.out.println("Output from java program");
         System.out.println(new Date());
         System.out.println("Created git repository");
+        System.out.println("I have successfully created Jenkins project. Thanks Durgesh!");
     }
 
 }
